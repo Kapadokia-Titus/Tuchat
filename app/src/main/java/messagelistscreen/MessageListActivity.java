58da223b63040b460176f7b3a6ea2b750a519cd3
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-08-05 02:06:57 +0300

    starter code

---
+++ app/src/main/java/messagelistscreen/MessageListActivity.java
@@ -0,0 +1,89 @@
+package messagelistscreen;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kapadokia.nyandoro.pluralchat.R;
+
+public class MessageListActivity extends AppCompatActivity {
+    public static final String EXTRA_CONTACT_NAME = "MessageListActivity.contactName";
+
+    private RecyclerView mMessageRecycler;
+    private MessageListAdapter mMessageAdapter;
+    private List<Message> mMessageList = new ArrayList<>();
+    private String sender;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_list);
+
+        this.sender = getIntent().getStringExtra(MessageListActivity.EXTRA_CONTACT_NAME);
+
+        setTitle(sender);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mMessageRecycler = (RecyclerView) findViewById(R.id.reyclerview_message_list);
+        mMessageRecycler.setLayoutManager(new LinearLayoutManager(this));
+        mMessageAdapter = new MessageListAdapter(this, mMessageList);
+        mMessageRecycler.setAdapter(mMessageAdapter);
+
+        updateMessageList();
+
+        findViewById(R.id.button_take_picture).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePicture();
+            }
+        });
+    }
+
+    private void takePicture(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 100);
+    }
+
+    public void updateMessageList(){
+        Message temp = new Message("Hiiii", "12:10pm", this.sender ,false);
+        mMessageList.add(temp);
+        temp = new Message("Hey", "12:11pm", this.sender ,true);
+        mMessageList.add(temp);
+        temp = new Message("What's the plan?", "12:14pm", this.sender ,false);
+        mMessageList.add(temp);
+        temp = new Message("You are coming to the party right?", "12:14pm", this.sender ,false);
+        mMessageList.add(temp);
+        temp = new Message("Yep! I am in", "12:20pm", this.sender ,true);
+        mMessageList.add(temp);
+        temp = new Message("Cool, Cya at my place then", "12:22pm", this.sender ,false);
+        mMessageList.add(temp);
+        temp = new Message("Same time", "12:30pm", this.sender ,false);
+        mMessageList.add(temp);
+        temp = new Message("Cya", "12:35pm", this.sender ,true);
+        mMessageList.add(temp);
+
+        mMessageAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // API 5+ solution
+                onBackPressed();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
